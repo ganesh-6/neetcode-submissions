19d9395e7f9fe8b2c8e562faaa2ecc0dd7ecd1ac
ganesh bhavar <ganeshbhawar347@gmail.com>
2026-08-24 13:21:11 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int ms = Math.min(word1.length(), word2.length());
+        String ans = "";
+        for(int i=0; i<ms; i++) {
+            ans+=word1.charAt(i);
+            ans+=word2.charAt(i);
+        }
+        if(word1.length() > word2.length()) {
+            ans = ans + word1.substring(ms,word1.length());
+        } else {
+            ans = ans + word2.substring(ms,word2.length());
+        }
+        return ans;
+    }
+}
